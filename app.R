7a681a496d0d7b2315736f1284fe8371791d0183
claud <claudia.montero@students.salle.url.edu>
2026-02-04 21:40:49 +0100

    2nd Iris Shiny App

--- app.R
+++ app.R
@@ -1,25 +1,57 @@
 library(shiny)
 
 ui <- fluidPage(
+  
   titlePanel("Iris Shiny App"),
+  
+  p("This Shiny application demonstrates reactive programming through univariate and multivariate visualizations of iris flower measurements."),
   
   sidebarLayout(
     sidebarPanel(
+      
+      h4("Selection"),
+      
       selectInput(
         "species",
-        "Select species:",
+        "Species:",
         choices = unique(iris$Species)
       ),
       
+      hr(),
+      
+      h4("Histogram"),
+      
       selectInput(
-        "variable",
-        "Select variable:",
+        "hist_var",
+        "Dimension:",
         choices = names(iris)[1:4]
+      ),
+      
+      hr(),
+      
+      h4("Scatter plot"),
+      
+      radioButtons(
+        "comparison_type",
+        "Compared dimensions:",
+        choices = c("Lengths", "Widths"),
+        selected = "Lengths"
       )
     ),
     
     mainPanel(
-      plotOutput("plot"),
+      
+      h4(textOutput("hist_title")),
+      plotOutput("histPlot"),
+      
+      br(),
+      
+      h4(textOutput("scatter_title")),
+      plotOutput("scatterPlot"),
+      
+      br(),
+      
+      h4("Data"),
       tableOutput("table")
     )
   )
@@ -31,11 +63,48 @@ server <- function(input, output) {
     iris[iris$Species == input$species, ]
   })
   
-  output$plot <- renderPlot({
+  output$hist_title <- renderText({
+    paste("Histogram for", input$species, "of", input$hist_var)
+  })
+  
+  output$histPlot <- renderPlot({
     hist(
-      data_filtered()[[input$variable]],
+      data_filtered()[[input$hist_var]],
       col = "lightblue",
-      main = input$variable
+      xlab = input$hist_var,
+      main = NULL
+    )
+  })
+  
+  output$scatter_title <- renderText({
+    if (input$comparison_type == "Lengths") {
+      paste("Scatter plot for ", input$species, "of Sepal.Length vs Petal.Length")
+    } else {
+      paste("Scatter plot for ", input$species, "of Sepal.Width vs Petal.Width")
+    }
+  })
+  
+  output$scatterPlot <- renderPlot({
+    
+    if (input$comparison_type == "Lengths") {
+      x <- data_filtered()$Sepal.Length
+      y <- data_filtered()$Petal.Length
+      xlab <- "Sepal.Length"
+      ylab <- "Petal.Length"
+    } else {
+      x <- data_filtered()$Sepal.Width
+      y <- data_filtered()$Petal.Width
+      xlab <- "Sepal.Width"
+      ylab <- "Petal.Width"
+    }
+    
+    plot(
+      x, y,
+      col = "darkgreen",
+      pch = 19,
+      xlab = xlab,
+      ylab = ylab,
+      main = NULL
     )
   })
   
